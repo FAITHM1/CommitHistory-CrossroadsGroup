e1f724f51fb214e908ec0e04587bafd11591227d
faith musa <faithy@faiths-MacBook-Air.local>
2022-05-21 23:13:22 -0500

    updated appJs by creating a get function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  const [commits, setCommits] = useState([]);
+  const url =
+    "https://api.github.com/repos/FAITHM1/CommitHistory-CrossroadsGroup/commits/";
+  const getCommits = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setCommits(data);
+  };
+  useEffect(() => {
+    getCommits();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
