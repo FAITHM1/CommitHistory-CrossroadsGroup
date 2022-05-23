e3f44b39ddb767defe5953c6d3e003e856a5be35
faith musa <faithy@faiths-MacBook-Air.local>
2022-05-22 19:20:22 -0500

    Created a recursive function, that pushes commits messages into an empty array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  //holds the state of the commits
   const [commits, setCommits] = useState([]);
+  //API url- going to hide the api url in a dotenv file
   const url =
     "https://api.github.com/repos/FAITHM1/CommitHistory-CrossroadsGroup/commits";
+
   const getCommits = async () => {
     const response = await fetch(url);
     const data = await response.json();
     setCommits(data);
   };
+
+  // recursive function
+  const comments = [];
+  const recursive = (num) => {
+    if (num > 0) {
+      comments.push(commits[num-1].commit.message);
+      recursive(num - 1);
+    }
+  };
+
   useEffect(() => {
-    console.log("its running");
     getCommits();
   }, []);
-
+  
+  recursive(commits.length);
+  console.log(comments);
   return <div className="App"></div>;
 }
 
