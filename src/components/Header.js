0b6ec332b5482bc81058cc9776288186a7870759
faith musa <faithy@faiths-MacBook-Air.local>
2022-05-22 20:21:40 -0500

    Finished with MVP

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="h1">Commit History</h1>
+    </header>
+  );
+}
+export default Header;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,18 +1,20 @@
+import Header from "../components/Header";
 function AllCommits({ comments }) {
   console.log(comments);
   return (
     <section className="list-group p-3">
+      <Header/>
       {comments.map((comment, index) => {
         if (index % 2 === 0) {
           return (
             <div key={index} className="list-group-item text-bg-dark p-3">
-              {comment.message}
+              {comment.message}.
             </div>
           );
         }else{
           return (
             <div key={index} className="list-group-item  p-3">
-              {comment.message}
+              {comment.message}.
             </div>
           );
         }
